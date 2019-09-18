ffd1cd0e27ee0a425af4acddae60547f50f9f80c
Jason Ostergren <jason.ostergren79@gmail.com>
2019-09-18 18:24:18 -0500

    updated with complete functionality

--- bamazon.js
+++ bamazon.js
@@ -3,6 +3,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const fs = require("fs")
+require("console.table")
 
 // defining a few more variables for user input
 
@@ -30,59 +31,130 @@ connection.connect(function (err) {
 
 function displayInventory() {
     console.log("Here's our store inventory for you to peruse!\n");
-    connection.query("SELECT * FROM products", function (err, res) {
+    const query = connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log(res);
-        connection.end();
-        chooseProduct()
+        console.table(res);
+        chooseProduct(res)
     });
 }
 
 // queries the user for the id of the product they'd like to purchase
 
-function chooseProduct() {
-    console.log("Please enter the id of the item you'd like to purchase!\n");
-    const query = connection.query("SELECT ProductID FROM products", function (err, res) {
+function chooseProduct(inventory) {
+
+    inquirer.prompt([
+
+                {
+                    type: "input",
+                    name: "name",
+                    message: "What is the product id that you'd like to purchase?"
+                }
+    ])
+
+    .then(function(val) {
+        const choiceid = parseInt(val.name)
+        const item = checkInventory(choiceid, inventory)
+        if (item) {
+            promptforquantity(item) 
+        } else {
+            console.log("I'm sorry we are out of this item!")
+            displayInventory()
+        }
+
+    })
+}
+
+function checkInventory(choiceid, inventory) {
+
+    for (let i = 0; i < inventory.length; i++) {
+      if (inventory[i].id===choiceid){
+          return inventory[i]
+      }
+    
+        
+    }
+return null
+}
+
+
+                
+function promptforquantity (item) {
+    inquirer.prompt([
+
+        {
+            type: "input",
+            name: "name",
+            message: "What is the quantity that you'd like to purchase?"
+        }
+])
+
+.then(function (val){
+    const quantity = parseInt(val.name)
+    console.log("item", item)
+    if (quantity > item.instock) {
+        console.log("We do not have enough in stock")
+        displayInventory()
+        
+        
+    } else {
+        finalizePurchase(item, quantity)
+
+
+    }
+
+
+})
+
+}
+
+function finalizePurchase (item, quantity) {
+    // UPDATE [table] SET [column] = '[updated-value]' WHERE [column] = [value];
+connection.query("UPDATE products SET instock = instock - ? WHERE id = ?",
+[quantity, item.id],
+function (err, res) {
         if (err) throw err;
-        console.log(res);
-        connection.end();
-        chooseQuantity ()
+        console.log("success");
+        displayInventory()
+
     });
 
 }
 
-// queries the user for the quantity of the id they'd like to purchase
 
-function chooseQuantity () {
-    console.log("Please enter the quantity of the item you'd like to purchase!\n");
-   
-}
 
-// checks inventory for availability 
 
-function checkInventory() {
-    console.log("Checking inventory to see if we have enough of your item in stock!\n");
- 
-    
-}
+                // // queries the user for the quantity of the id they'd like to purchase
 
-// updates product
+                // function chooseQuantity () {
+                //     console.log("Please enter the quantity of the item you'd like to purchase!\n");
 
-function updateProduct() {
-    console.log("Updating product quantities...\n");
-    var query = connection.query(
-      "UPDATE products SET ? WHERE ?",
-      [
-        {
-          quantity: 100
-        },
-        {
-          flavor: "Rocky Road"
-        }
-      ],
-      function(err, res) {
-        if (err) throw err;
-        console.log(res.affectedRows + " products updated!\n");
-        deleteProduct();
-      }
-    );
+                // }
+
+                // // checks inventory for availability 
+
+                // function checkInventory() {
+                //     console.log("Checking inventory to see if we have enough of your item in stock!\n");
+
+
+                // }
+
+                // // updates product
+
+                // function updateProduct() {
+                //     console.log("Updating product quantities...\n");
+                //     var query = connection.query(
+                //       "UPDATE products SET ? WHERE ?",
+                //       [
+                //         {
+                //           quantity: 100
+                //         },
+                //         {
+                //           flavor: "Rocky Road"
+                //         }
+                //       ],
+                //       function(err, res) {
+                //         if (err) throw err;
+                //         console.log(res.affectedRows + " products updated!\n");
+                //         deleteProduct();
+                //       }
+                //     );
